b783901ca84d7e4c3de1aa327f1a6a79856da60c
Baltyr <72105167+Baltyr@users.noreply.github.com>
2022-11-21 15:59:57 -0300

    conexion con express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-console.log("hola mundo")
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// importamos conexión db
+const sequelize = requiere ('../database/db.js')
+
+
+// rutas 
+app.get("/", (req, res) => {
+  res.send("Hola mundo ");
+});
+
+
+// arrancamos el sv 
+app.listen(port, () => {
+  console.log(`Escuchando en el puerto ${port}`);
+});
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
